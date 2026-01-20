bd5f9995cd307f104f9ff08e318adae0a7f646c9
cristian <alexander85vega@gmail.com>
2026-01-20 20:57:08 -0300

    feat: Enhance language selection in footer component for improved accessibility; filter language options and add hidden label

--- public/js/footer.js
+++ public/js/footer.js
@@ -1,10 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
   const languageSelect = document.getElementById("language");
 
-  languageSelect.addEventListener("change", (event) => {
-    console.log("Language changed to:", event.target.value);
-    const selectedLanguage = event.target.value;
-    console.log("/" + selectedLanguage);
-    window.location.href = `/${selectedLanguage}`;
-  });
+  if (languageSelect) {
+    const availableLangs = ["es", "en"];
+    const currentPathSegments = window.location.pathname.split("/").filter(Boolean);
+    const currentLang = availableLangs.includes(currentPathSegments[0]) ? currentPathSegments[0] : "es";
+    languageSelect.value = currentLang;
+
+    languageSelect.addEventListener("change", (e) => {
+      const selectedLang = e.target.value;
+      const currentPath = window.location.pathname;
+      const pathSegments = currentPath.split("/").filter(Boolean);
+
+      const defaultLang = "es";
+
+      if (availableLangs.includes(pathSegments[0])) {
+        pathSegments.shift();
+      }
+
+      const remainingPath = pathSegments.join('/');
+      let newPath = '';
+
+      if (selectedLang !== defaultLang) {
+        newPath = `/${selectedLang}`;
+      }
+
+      if (remainingPath) {
+        newPath = `${newPath}/${remainingPath}`;
+      }
+
+      if (newPath === '') {
+        newPath = '/';
+      }
+      window.location.href = newPath;
+    });
+  }
 });
